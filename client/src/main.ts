6d94bee2a5280294f07c6e23fb3ccd819e16a1b7
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-03-30 19:18:10 -0700

    feat(client): initial setup
    Set up the initial Sentry config and deployment tool.

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,10 +3,24 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import * as Sentry from '@sentry/angular';
+import { Integrations } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: 'https://18d930653a9f42bbbfca49ed16a46f53@o485764.ingest.sentry.io/5700082',
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracingOrigins: ['localhost', 'https://fcc-projects.nhcarrigan.com'],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
