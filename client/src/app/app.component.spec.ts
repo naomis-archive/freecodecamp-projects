e9646684bfac3c9ce8b2c8d9180db6c28ac23db1
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-04-12 17:27:42 -0700

    feat: tribute page tests
    Add tests to represent the user stories required by the project.

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'freecodecamp-projects-client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('freecodecamp-projects-client');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('client app is running!');
-  });
 });
--- client/src/app/tribute/tribute.component.spec.ts
+++ client/src/app/tribute/tribute.component.spec.ts
@@ -5,21 +5,71 @@ import { TributeComponent } from './tribute.component';
 describe('TributeComponent', () => {
   let component: TributeComponent;
   let fixture: ComponentFixture<TributeComponent>;
+  let compiled: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TributeComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
+      declarations: [TributeComponent],
+    }).compileComponents();
     fixture = TestBed.createComponent(TributeComponent);
     component = fixture.componentInstance;
+    compiled = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the main element (User Story 1)', () => {
+    const main = compiled.querySelector('#main');
+    expect(main).toBeDefined('missing the main element');
+    // if main is root element for component, top level should be the ng-version
+    expect(main.parentElement.getAttribute('ng-version')).toBeDefined(
+      'main element does not contain all other elements'
+    );
+  });
+
+  it('should have an element with id of title (User Story 2)', () => {
+    const title = compiled.querySelector('#title');
+    expect(title).toBeDefined('missing the title element');
+  });
+
+  it('should have an img-div element (User Story 3)', () => {
+    const imgDiv = compiled.querySelector('#img-div');
+    expect(imgDiv).toBeDefined('missing img-div element');
+  });
+
+  it('should have an image element (User Story 4)', () => {
+    const image = compiled.querySelector('#image');
+    expect(image).toBeDefined('missing image element');
+    expect(image.tagName).toBe('IMG', 'image element is not an img element');
+    expect(image.parentElement.getAttribute('id')).toBe(
+      'img-div',
+      'image element is not within img-div'
+    );
+  });
+
+  it('should have an img-caption element (User Story 5)', () => {
+    const imgCaption = compiled.querySelector('#img-caption');
+    expect(imgCaption).toBeDefined('missing img-caption element');
+    expect(imgCaption.tagName).toBe(
+      'FIGCAPTION',
+      'img-caption is not a figcaption'
+    );
+    expect(imgCaption.parentElement.getAttribute('id')).toBe(
+      'img-div',
+      'img-caption is not within the img-div element'
+    );
+  });
+
+  it('should have a tribute-info element (User Story 6)', () => {
+    const tributeInfo = compiled.querySelector('#tribute-info');
+    expect(tributeInfo).toBeDefined('missing tribute-info element');
+  });
+
+  it('should have a tribute-link element (User Story 7)', () => {
+    const tributeLink = compiled.querySelector('#tribute-link');
+    expect(tributeLink).toBeDefined('missing tribute-link element');
   });
 });
