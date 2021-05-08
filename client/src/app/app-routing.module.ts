eb02386a20d13d65661ab0e0464d0c130cbd97be
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-05-08 16:14:28 -0700

    feat: product landing page
    Add a basic product landing page.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './responsive-web-design/product/product.component';
 import { SurveyComponent } from './responsive-web-design/survey/survey.component';
 import { TributeComponent } from './responsive-web-design/tribute/tribute.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'rwd/tribute', pathMatch: 'full', component: TributeComponent },
   { path: 'rwd/survey', pathMatch: 'full', component: SurveyComponent },
+  { path: 'rwd/product', pathMatch: 'full', component: ProductComponent },
 ];
 
 @NgModule({
